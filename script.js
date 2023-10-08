19f8448d03d97e9139d1861907480411349b5893
fpS-AUDIO <saudio.ru@gmail.com>
2023-10-08 22:19:16 +0200

    updated some features

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 `use strict`;
 
+///////////// ALL GLOBAL VARIABLES /////////////
+
 const inputArea = document.querySelector(`.userInput`);
 const resultText = document.querySelector(`.resultText`);
 const btnUpperCase = document.querySelector(`.UpperCase`);
+const btnLowerCase = document.querySelector(`.LowerCase`);
+const btnCapitalize = document.querySelector(`.Capitalize`);
+const btnCamelCase = document.querySelector(`.CamelCase`);
+const btnUnderscore_Case = document.querySelector(`.Underscore_Case`);
 
-/* let userInputText = ``;
-let transformeduserInputText = ``; */
+///////////// MAIN FUNCTIONS /////////////
 
 const getUserInput = function () {
   return inputArea.value;
@@ -16,10 +21,62 @@ const upperCase = function () {
   return userInput.toUpperCase();
 };
 
+const lowerCase = function () {
+  const userInput = getUserInput();
+  return userInput.toLowerCase();
+};
+
+const capitalize = function () {
+  const wordCapitalized = [];
+  const userInput = getUserInput();
+  const userInputLower = userInput.toLowerCase().trim();
+  const wordsArray = userInputLower.split(` `);
+
+  // create new array wordsArrayTrue without empty values
+  const wordsArrayTrue = wordsArray.filter(Boolean);
+
+  // push() to empty "wordCapitalized" array changed elements of "wordsArray"
+  for (const word of wordsArrayTrue) {
+    wordCapitalized.push(word.replace(word[0], word[0].toUpperCase()));
+  }
+  return wordCapitalized.join(` `);
+};
+
+const camelCase = function () {
+  const userInput = getUserInput();
+
+  // count spaces (` `)
+  // count underscores (_)
+  // if more spaces replace spaces capitalize and remove spaces
+  // if more underscore capitalize and remove underscores
+
+
+  // count values in an array:
+  /*
+  function getOccurrence(array, value) {
+    return array.filter((v) => (v === value)).length;
+  */
+}
+
+console.log(getOccurrence(arr, 1));  // 2
+console.log(getOccurrence(arr, 3));  // 3
+};
+
+///////////// EVENT LISTENERS /////////////
+
 btnUpperCase.addEventListener(`click`, function () {
   const userInputUpper = upperCase();
-  console.log(userInputUpper);
   resultText.textContent = userInputUpper;
+});
+
+btnLowerCase.addEventListener(`click`, function () {
+  const userInputLower = lowerCase();
+  resultText.textContent = userInputLower;
+});
+
+btnCapitalize.addEventListener(`click`, function () {
+  const userInputCapitalized = capitalize();
+  resultText.textContent = userInputCapitalized;
 });
 /* Write a program that receives a list of variable names written in underscore_case
 and convert them to camelCase.
